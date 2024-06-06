49c27856b74b5f6e0f6e6e94ed8d3ead45b0cb29
Matthias1221 <mathias.gherasim@ulbsibiu.ro>
2024-06-06 16:05:21 +0300

    Initial commit

---
+++ crud_app/server/model/model.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require("mongoose")
+
+var schema = new mongoose.Schema({
+name:{
+    type:String,
+    required:true
+},
+email:{
+    type:String,
+    required:true,
+    unique:true
+},
+gender:String,
+status:String
+})
+
+const Userdb = mongoose.model('userdb',schema)  //userdb is the name of the collection in the database
+
+module.exports=Userdb;
