2e255edf1a9decc601b9a8b4e4b6b7db086cf05a
Matthias1221 <mathias.gherasim@ulbsibiu.ro>
2024-06-07 09:52:32 +0300

    x

--- crud_app/assets/js/index.js
+++ crud_app/assets/js/index.js
@@ -47,3 +47,21 @@ if(window.location.pathname == "/"){
 
     })
 }
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDb4olCuli0jikUrHFOwmmc3JD3qRbo-b4",
+    authDomain: "mansis-d1b0b.firebaseapp.com",
+    projectId: "mansis-d1b0b",
+    storageBucket: "mansis-d1b0b.appspot.com",
+    messagingSenderId: "996183535452",
+    appId: "1:996183535452:web:92423cc989a02f76d21b99"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
